32b5bca6a76165e09e79cc3e57122aaf418de817
maulana <maulana20@versatech.co.id>
2020-04-14 12:20:04 +0700

    revisi package.json
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+#!/usr/bin/env node
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+	handleError(`UNHANDLED ERROR`, err);
+});
+
 const cli = require('./utils/cli.js');
 const Museum = require('./data/museum.js');
 const Region = require('./data/region.js');
